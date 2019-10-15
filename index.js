f3c094c7496efbe4d2064a29653683c6b437242c
Josué Dïaz <josuecdiaz.319@gmail.com>
2019-10-15 11:55:21 -0300

    tercer commit

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const server = require('http').Server(app);
 
 //app.use(express.static(__dirname + '/public'));
 
+app.use(express.static('/css'));
+
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/example/', function (req, res) {
+  res.sendFile(__dirname + '/example.html');
+});
+
+app.get('/css/style.css', function (req, res) {
+    console.log('Get: css/style.css');
+    res.sendFile('/css/style.css', {root: __dirname })
+})
+
+app.get('/simplegame/', function (req, res) {
+  res.sendFile(__dirname + '/simplegame.html');
 });
 
 server.listen(8080, function () {
