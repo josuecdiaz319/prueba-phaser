b32855bcfed4bbfaf906f0d8d21d3a3aa662cdb6
Mugi <mugiriven@gmail.com>
2019-10-14 22:02:49 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
